2fce665c0ab5cb139be091bb9ad134283a1bc253
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-02-27 00:19:06 +0100

    Create log.lua

---
+++ log.lua
@@ -0,0 +1,37 @@
+local index = {}
+
+function index:log(str,type)
+    type = type or "info"
+    if self.lastLog == str..type then
+        self.nstr = self.nstr + 1
+        local x,y = self.term.getCursorPos()
+        self.term.setCursorPos(x,y-1)
+        self.term.clearLine()
+    end
+    self.lastLog = str..type
+    local width = self.term.getSize()
+    local timeStr = "["..textutils.formatTime(os.time()).."] "
+    local tb,tt = self.term.getBackgroundColor(),self.term.getTextColor()
+    if type == "error" then self.term.setTextColor(colors.red)
+    elseif type == "warn" then self.term.setTextColor(colors.yellow)
+    elseif type == "fatal" then self.term.setBackgroundColor(colors.red)
+    elseif type == "sucess" then self.term.setBackgroundColor(colors.lime)
+    elseif type == "message" then self.term.setTextColor(colors.white)
+    elseif type == "update" then self.term.setTextColor(colors.green)
+    elseif type == "info" then self.term.setTextColor(colors.gray)
+    end
+    local len = #str+#timeStr+#("("..tostring(self.nstr)..")")
+    if len < 2 then len = 2 end
+    print(timeStr..str..(" "):rep(width-len).."("..tostring(self.nstr)..")")
+    self.term.setBackgroundColor(tb);self.term.setTextColor(tt)
+end
+
+local function createLog(termObj)
+    return setmetatable({
+        lastLog="",
+        nstr=1,
+        term=termObj
+    },{__index=index})
+end
+
+return {create_log=createLog}
