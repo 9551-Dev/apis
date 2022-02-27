499f6b9f2a815f2294934dbf4f7d80c749b392e3
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-02-27 17:00:13 +0100

    Update log.lua

--- log.lua
+++ log.lua
@@ -3,10 +3,10 @@ local index = {}
 local function writeWrapped(termObj,str)
     local width = termObj.getSize()
     local strings,maxLen = {},math.ceil(#str/width)
-    local last = 1
+    local last = 0
     for i=1,maxLen do
         local _,y = term.getCursorPos()
-        termObj.write(str:sub(last,i*width))
+        termObj.write(str:sub(last+1,i*width))
         termObj.setCursorPos(1,y+1)
         last=i*width
     end
@@ -36,9 +36,9 @@ function index:log(str,type)
     elseif type == "info" then self.term.setTextColor(colors.gray)
     else self.term.setTextColor(colors.magenta) end
     local len = #str+#timeStr+#("("..tostring(self.nstr)..")")
-    if len < 2 then len = 2 end
+    if len < 1 then len = 1 end
     local wlen = width-len
-    if wlen < 1 then wlen = 1 end
+    if wlen < 2 then wlen = 1 end
     self.maxln = writeWrapped(self.term,timeStr..str..(" "):rep(wlen).."("..tostring(self.nstr)..")")
     self.term.setBackgroundColor(tb);self.term.setTextColor(tt)
 end
