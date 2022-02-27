246c5086ce44c9cc78a6b5fb3967bb7ccce10d93
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-02-27 17:59:35 +0100

    Update log.lua

--- log.lua
+++ log.lua
@@ -5,12 +5,25 @@ local function writeWrapped(termObj,str)
     local strings,maxLen = {},math.ceil(#str/width)
     local last = 0
     for i=1,maxLen do
-        local _,y = term.getCursorPos()
+        local _,y = termObj.getCursorPos()
         termObj.write(str:sub(last+1,i*width))
         termObj.setCursorPos(1,y+1)
         last=i*width
     end
     return maxLen
+end
+
+local function getLineLen(termObj,str)
+    local width = termObj.getSize()
+    local strings,maxLen = {},math.ceil(#str/width)
+    local last = 0
+    local strLen
+    for i=1,maxLen do
+        local strs = str:sub(last+1,i*width)
+        last=i*width
+        strLen = #strs
+    end
+    return strLen
 end
 
 function index:log(str,type)
@@ -35,10 +48,12 @@ function index:log(str,type)
     elseif type == "update" then self.term.setTextColor(colors.green)
     elseif type == "info" then self.term.setTextColor(colors.gray)
     else self.term.setTextColor(colors.magenta) end
-    local len = #str+#timeStr+#("("..tostring(self.nstr)..")")
+    local strse = timeStr..str.."("..tostring(self.nstr)..")"
+    local len = #strse
     if len < 1 then len = 1 end
     local wlen = width-len
-    if wlen < 2 then wlen = 1 end
+    if wlen < 1 then wlen = 1 end
+    wlen = width-(getLineLen(self.term,strse))
     self.maxln = writeWrapped(self.term,timeStr..str..(" "):rep(wlen).."("..tostring(self.nstr)..")")
     self.term.setBackgroundColor(tb);self.term.setTextColor(tt)
 end
